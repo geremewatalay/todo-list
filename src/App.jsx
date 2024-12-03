929ffdc59d4a0da74c8b35ec15a3312241854f4e
geremewatalay <geremewatalay@gmail.com>
2024-12-03 03:33:39 -0800

    styles commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 
+import "./styles.css"
+
 
 function App() {
   const [count, setCount] = useState(0)
 
-  return (
-    <>
-    </>
-  )
+  return <form className="new-item-form">
+    <div>
+      <label htmlFor="item">New Item</label>
+      <input type="text" id=""/>
+    </div>
+    <button className="btn">Add</button>
+  </form>
 }
 
 export default App
